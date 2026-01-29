bfdf0526d5425c8dc1ce4d7450d8e1a899b1d32c
Gloria NeuroCinematic <gloriacitizen.00@gmail.com>
2026-01-28 20:26:29 -0600

    Update fireworks1.py

--- fireworks1.py
+++ fireworks1.py
@@ -1,37 +1,29 @@
 import turtle
 import random
 
+screen = turtle.Screen()
+screen.bgcolor("black")
 
-turtle.Screen().bgcolor("black")
 t = turtle.Turtle()
 t.speed(0)
-
-
-def pen_colour(color):
-    t.color(color)
-
+t.hideturtle()
 
 def fireworks(distance):
     for _ in range(20):
         t.forward(distance)
         t.right(180 - (360 / 20))
 
-
 def move():
     t.penup()
-    x = random.randint(-300, 300)
-    y = random.randint(-300, 300)
-    t.goto(x, y)
+    t.goto(random.randint(-300,300), random.randint(-300,300))
     t.pendown()
 
-
-colors = ['red', 'purple', 'green', "orange", "yellow", "cyan"]
-
+colors = ['red', 'purple', 'green', 'orange', 'yellow', 'cyan']
 
 for _ in range(30):
-    pen_colour(random.choice(colors))
+    t.color(random.choice(colors))
     fireworks(random.randint(50, 100))
     move()
 
-turtle.done()
+turtle.exitonclick()
 
